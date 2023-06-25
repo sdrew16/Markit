f3f790940a059ac281f6d1f543e6f9239e5bfc8e
Shannon Drew <shannon.nicole.drew@gmail.com>
2023-06-25 12:13:18 -0700

    scaffolded user and event

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :events
   get 'hello/index'
   root 'hello#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
