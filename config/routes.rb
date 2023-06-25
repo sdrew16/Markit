a82768bac272a6d97f6795414213fc112c8e7c15
Shannon Drew <shannon.nicole.drew@gmail.com>
2023-06-25 13:20:58 -0700

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,4 @@ Rails.application.routes.draw do
   resources :events
   get 'events/index'
   root 'events#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
